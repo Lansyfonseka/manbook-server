1f98a394c629810dd5357702ec7bfd5143d02768
Lansyfonseka <twinkstriker@gmail.com>
2022-05-19 14:34:06 +0300

    fix: connect to database

--- models/index.js
+++ models/index.js
@@ -6,9 +6,9 @@ const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'production';
 const config = require(__dirname + '/../config/config.json')[env];
-const congifDeploy = require('../server.config');
 const db = {};
 console.log(env);
+config.dialectModule = require('mysql2')
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
