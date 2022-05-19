d06b69d5a226901f5f485cf36bb4bbdf347867b1
Lansyfonseka <twinkstriker@gmail.com>
2022-05-19 10:49:44 +0300

    try: check deploy server

--- server.js
+++ server.js
@@ -1,19 +1,32 @@
-const express = require('express');
-const cors = require('cors');
-const db = require('./models');
+// const express = require('express');
+// const cors = require('cors');
+// const { application } = require('express');
+
+// const db = require('./models');
+
+// const PORT = process.env.PORT || 5000;
+// const app = express();
+
+// app.use(express.urlencoded({extended:true}));
+// app.use(express.json());
+// app.use(cors());
+
+// const apiRoutes = require('./routes/apiRoutes');
+// app.use('/api',apiRoutes);
+
+// db.sequelize.sync().then( () => {
+//   app.listen(PORT, () => {
+//     console.log(`Listening on: http://localhost:${PORT}`)
+//   })
+// })
+
+const express = require("express");
+const app = express();
+const product = require("./routes/apiRoutes");
+
+app.use(express.json({ extended: false }));
+
+app.use("/api/users", product);
 
 const PORT = process.env.PORT || 5000;
-const app = express();
-
-app.use(express.urlencoded({extended:true}));
-app.use(express.json());
-app.use(cors());
-
-const apiRoutes = require('./routes/apiRoutes');
-app.use('/api',apiRoutes);
-
-db.sequelize.sync().then( () => {
-  app.listen(PORT, () => {
-    console.log(`Listening on: http://localhost:${PORT}`)
-  })
-})
+app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
