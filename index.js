5ded84ed36dc25534aeac77a3ccc5797c1b4b471
Lansyfonseka <twinkstriker@gmail.com>
2022-05-19 11:17:16 +0300

    fix: connect to database

--- index.js
+++ index.js
@@ -1,32 +1,21 @@
-// const express = require('express');
-// const cors = require('cors');
-// const { application } = require('express');
+const express = require('express');
+const cors = require('cors');
+const { application } = require('express');
 
-// const db = require('./models');
-
-// const PORT = process.env.PORT || 5000;
-// const app = express();
-
-// app.use(express.urlencoded({extended:true}));
-// app.use(express.json());
-// app.use(cors());
-
-// const apiRoutes = require('./routes/apiRoutes');
-// app.use('/api',apiRoutes);
-
-// db.sequelize.sync().then( () => {
-//   app.listen(PORT, () => {
-//     console.log(`Listening on: http://localhost:${PORT}`)
-//   })
-// })
-
-const express = require("express");
-const app = express();
-const product = require("./routes/apiRoutes");
-
-app.use(express.json({ extended: false }));
-
-app.use("/api/users", product);
+const db = require('./models');
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
+const app = express();
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(cors());
+
+const apiRoutes = require('./routes/apiRoutes');
+app.use('/api',apiRoutes);
+
+db.sequelize.sync().then( () => {
+  app.listen(PORT, () => {
+    console.log(`Listening on: http://localhost:${PORT}`)
+  })
+})
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,76 +1,59 @@
-const express = require("express");
-const router = express.Router();
+const express = require('express');
+const { mainModule } = require('process');
+const router = express.Router()
+const db = require("../models");
 
-router.get("/", async (req, res) => {
-  try {
-    res.json({
-      status: 200,
-      message: "Get data has successfully",
-    });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).send("Server error");
-  }
+router.get('/users',(req,res) => {
+  db.Users.findAll({
+    attributes: ['id','name','mail','active','createdAt','updatedAt','photo']
+  }).then(users => res.send(users))
+  // res.json({
+  //   status: 200,
+  //   message: "It's working good!"
+  // })
+});
+
+router.post('/register',(req,res)=>{
+  const {name,mail,password} = req.body;
+  db.Users.create({
+    name: name,
+    mail: mail,
+    password: getSHA256(password, name)
+  }).then((response)=>res.send(response));
+});
+
+router.post('/login', (req,res)=> {
+  const {mail,password} = req.body;
+  db.Users.findAll({
+    attributes: ['name', 'password', 'id'],
+    where: {
+      mail: mail,
+      password: getSHA256(password)
+    }
+  }).then(response=>{
+    if (!response.length)
+      res.status(403).send({
+        status: 403,
+        message:'Invalid mail or password'
+      });
+    else {
+      const {name,mail,id} = response.pop();
+      res.send({
+        status: '200',
+        user: {
+          name: name,
+          mail: mail,
+          id: id
+        }
+      })
+    }
+  })
 });
 
 module.exports = router;
 
-// const express = require('express');
-// const { mainModule } = require('process');
-// const router = express.Router()
-// const db = require("../models");
-
-// router.get('/users',(req,res) => {
-//   db.Users.findAll({
-//     attributes: ['id','name','mail','active','createdAt','updatedAt','photo']
-//   }).then(users => res.send(users))
-//   // res.json({
-//   //   status: 200,
-//   //   message: "It's working good!"
-//   // })
-// });
-
-// router.post('/register',(req,res)=>{
-//   const {name,mail,password} = req.body;
-//   db.Users.create({
-//     name: name,
-//     mail: mail,
-//     password: getSHA256(password, name)
-//   }).then((response)=>res.send(response));
-// });
-
-// router.post('/login', (req,res)=> {
-//   const {mail,password} = req.body;
-//   db.Users.findAll({
-//     attributes: ['name', 'password', 'id'],
-//     where: {
-//       mail: mail,
-//       password: getSHA256(password)
-//     }
-//   }).then(response=>{
-//     if (!response.length)
-//       res.status(403).send({
-//         status: 403,
-//         message:'Invalid mail or password'
-//       });
-//     else {
-//       const {name,mail,id} = response.pop();
-//       res.send({
-//         status: '200',
-//         user: {
-//           name: name,
-//           mail: mail,
-//           id: id
-//         }
-//       })
-//     }
-//   })
-// });
-
-// module.exports = router;
-
-// function getSHA256(value, key) {
-//   const { createHmac } = require('node:crypto');
-//   const hash = createHmac('sha256',key).update(value).digest('hex');
-//   return hash
-// }
+function getSHA256(value, key) {
+  const { createHmac } = require('node:crypto');
+  const hash = createHmac('sha256',key).update(value).digest('hex');
+  return hash
+}
